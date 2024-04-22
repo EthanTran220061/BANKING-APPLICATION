32b57f22f9e0547d40ae1514b23a245eaff28ed6
Ethan Tran <etran628@gmail.com>
2024-04-22 14:51:05 -0700

    Add project files.

---
+++ Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANKING_APPLICATION
+{
+    abstract class Account
+    {
+        private int accountNumber;
+        protected double balance;
+        /// <summary>
+        /// Constructor with account number
+        /// </summary>
+        /// <param name="accountNumber"></param>
+        
+        public Account(int accountNumber)
+        {
+            this.accountNumber = accountNumber;
+            balance = 0;
+        }
+
+        /// <summary>
+        /// Constructor with account number and initial balance
+        /// </summary>
+        /// <param name="accountNumber"></param>
+        /// <param name="initBalance"></param>
+        public Account(int accountNumber, double initBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = initBalance;
+        
+        }
+    }
+}
---
+++ BankForm.cs
@@ -0,0 +1,15 @@
+namespace BANKING_APPLICATION
+{
+    public partial class BankForm : Form
+    {
+        public BankForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
